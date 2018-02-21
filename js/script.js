99d8e11d213c56cf3f7906d2e8383e0a6749fe42
Elżbieta Kukułka <ellakaras@gmail.com>
2018-02-21 17:19:17 +0100

    Add comments in scripts.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name); //Comment about alert
+console.log('Hello, ' + name);
+/* Another comment
+which are longer than 
+the first one
+*/
